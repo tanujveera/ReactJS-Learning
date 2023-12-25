ecf70d8cbe8847c1edd6f50c77f236d086077a16
tanujveera <tanuj.veera@gmail.com>
2023-12-25 22:57:35 +0530

    Day1 of React

---
+++ Day 1/App.js
@@ -0,0 +1,8 @@
+//To create a html tag
+// const heading = React.createElement("h1",{id: "heading"},"Hello World from React");
+const htag = React.createElement("h1",{},"H1 Tag");
+const htag2 =React.createElement("h2",{},"H2 Tag");
+const child = React.createElement("div",{id:"child"},[htag,htag2]);
+const parent = React.createElement("div",{id:"parent"},[child,child]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
