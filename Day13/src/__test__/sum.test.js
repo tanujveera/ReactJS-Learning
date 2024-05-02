fcd351caad69c1587ee04d950b98b1a34418ce2e
tanujveera <tanuj.veera@gmail.com>
2024-05-02 11:46:23 +0530

    Coverage

--- Day13/src/__test__/sum.test.js
+++ Day13/src/__test__/sum.test.js
@@ -1,8 +1,15 @@
-import { sum } from "../components/sum"
+import { sum } from "../components/sum";
 
-test("Sum Function should calculate sum of numbers",()=>{
-  const result = sum(2,3);
+test("Sum Function should calculate sum of numbers", () => {
+  const result = sum(2, 3);
 
   //Assertion
   expect(result).toBe(5);
-})
+});
+
+test("Sum Function should calculate sum of numbers", () => {
+  const result = sum(5, 3);
+
+  //Assertion
+  expect(result).toBe(8);
+});
