b50cc543ae543322877e958b961f2065abbc8306
tanujveera <tanuj.veera@gmail.com>
2024-05-06 15:07:16 +0530

    Config

--- Day13/src/__test__/Contect.test.js
+++ Day13/src/__test__/Contect.test.js
@@ -1,10 +1,11 @@
 import {render,screen} from "@testing-library/react"
 import Contact from "../components/Contact"
+import   "@testing-library/jest-dom"
 
 test("Should load contact as component",()=>{
   render(<Contact/>);
 
-  const heading = screen.getByRole("heading");
+  const heading = screen.getByRole("reason");
 
-  expect(heading).toBeInTheDocument();
+  expect(heading).toBe
 });
